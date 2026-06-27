97e49df9567e1f6cbaa0b2303dbad872901b6a99
Fabian8907 <111251858+Fabian8907@users.noreply.github.com>
2026-06-26 19:29:58 -0500

    Delete gimnasio_naza directory

--- gimnasio_naza/gimnasio/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class GimnasioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gimnasio'
-    def ready(self):
-        import gimnasio.signals
